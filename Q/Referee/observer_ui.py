dc1e28f0db41ad9dd910c7cc2fff8dc0de490cb5
echase20 <chase.ev@northeastern.edu>
2023-11-09 13:18:39 -0500

    Update observer_ui.py
    fixed UI

--- Q/Referee/observer_ui.py
+++ Q/Referee/observer_ui.py
@@ -1,11 +1,6 @@
-from tkinter import Tk, ttk
-from PIL import ImageTk, Image
-from PyQt5.QtWidgets import (
-    QApplication,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+from tkinter import Tk, Label, Button
+
+from PIL import ImageTk
 
 from Q.Referee.next_states import nextState
 from Q.Referee.observer_ui_callbacks import ObserverUICallback
@@ -17,35 +12,26 @@ class ObserverUI:
         self.current_state = 0
         self.callback = callback
 
-        app = QApplication([])
-        window = QWidget()
-        window.setWindowTitle("QVBoxLayout")
+        self.root = Tk()
+        self.root.geometry("800x600")
+        img = ImageTk.PhotoImage(file="../../8/tmp/0.png")
+        #imgTk = PhotoImage(img)
+        self.board = Label(self.root, image=img)
+        self.board.photo = img
+        self.board.grid(row=1)
+        self.next_button = Button(text="Next", command=self.switch_next)
+        self.next_button.grid(row=0, column=0)
+        self.previous_button = Button(text="Previous", command=self.switch_prev)
+        self.previous_button.grid(row=0, column=1)
+        self.save_button = Button(text="Save", command=callback.save_jstate).grid(row=0, column=2)
 
-        layout = QVBoxLayout()
-        layout.addWidget(QPushButton("Top"))
-        layout.addWidget(QPushButton("Center"))
-        layout.addWidget(QPushButton("Bottom"))
-        window.setLayout(layout)
-        window.show()
-        app.exec()
-        """
-        root = Tk()
-        root.geometry("800x600")
-        self.map = ttk.Label()
-        self.map.grid(row=0)
-        self.next_button = ttk.Button(text="Next", command=self.switch_next)
-        self.next_button.grid(row=1, column=0)
-        self.previous_button = ttk.Button(text="Previous", command=self.switch_prev)
-        self.previous_button.grid(row=1, column=1)
-        self.save_button = ttk.Button(text="Save", command=callback.save_jstate).grid(row=1, column=2)
-        while True:
-            root.update_idletasks()
-            root.update()
-        """
 
-    def receive_new_image(self, img: Image):
-        imgTk = ImageTk.PhotoImage(img)
-        self.map = ttk.Label(imgTk)
+
+    def receive_new_image(self, filename: str):
+        img = ImageTk.PhotoImage(file="../../8/tmp/" + filename)
+        img.photo = img
+        #imgTk = PhotoImage(img)
+        self.board.configure(image=img)
 
     def switch_prev(self):
         prev_button_state = self.callback.isPrevious(self.current_state)
@@ -62,7 +48,6 @@ class ObserverUI:
             self.callback.switch(self.current_state)
         if next_button_state == nextState.END:
             self.next_button.configure(state="disabled")
-        if next_button_state == nextState.WAITING:
-            print("MORE STATES INCOMING")
 
-
+    def runUI(self):
+        self.root.mainloop()
