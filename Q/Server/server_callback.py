83e65345169bad3336704c4bd6dfaefce7633898
DilanRocco <captaindilan12@gmail.com>
2023-11-22 21:48:58 -0500

    add correct return types for ending a game in the server

--- Q/Server/server_callback.py
+++ Q/Server/server_callback.py
@@ -1,11 +1,14 @@
+import json
 from typing import Dict
 
 from Q.Referee.referee import Referee
 from Q.Server.player import ProxyPlayer
+from Q.Util.util import Util
 
 
 class ServerCallbacks():
     def start_game(self, names: Dict):
         ref = Referee()
         player_list = [ProxyPlayer(name, conn) for name, conn in names.items()]
-        print(ref.main(player_list))
+        res = ref.main(player_list)
+        print(json.dumps(Util().pair_results_to_jresults(res)))
